aa6a0f7093beab3d7fcc65906a4309e28b661e07
vietlong1011 <longlieuliny@gmail.com>
2024-02-21 16:59:06 +0700

    v1.3 DB

---
+++ shop.sql
@@ -0,0 +1,194 @@
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: shop_restful
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `items`
+--
+
+DROP TABLE IF EXISTS `items`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items` (
+  `id_items` bigint NOT NULL AUTO_INCREMENT,
+  `create_by` varchar(255) DEFAULT NULL,
+  `create_date` datetime(6) DEFAULT NULL,
+  `last_modified_by` varchar(255) DEFAULT NULL,
+  `last_modified_date` datetime(6) DEFAULT NULL,
+  `made` varchar(255) NOT NULL,
+  `name_items` varchar(255) NOT NULL,
+  `price` bigint NOT NULL,
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`id_items`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items`
+--
+
+LOCK TABLES `items` WRITE;
+/*!40000 ALTER TABLE `items` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `items_aud`
+--
+
+DROP TABLE IF EXISTS `items_aud`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `items_aud` (
+  `id_items` bigint NOT NULL,
+  `rev` int NOT NULL,
+  `revtype` tinyint DEFAULT NULL,
+  `create_date` datetime(6) DEFAULT NULL,
+  `made` varchar(255) DEFAULT NULL,
+  `name_items` varchar(255) DEFAULT NULL,
+  `price` bigint DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  PRIMARY KEY (`rev`,`id_items`),
+  CONSTRAINT `FK9btc5ke94st1j6hgxpjj9ldwp` FOREIGN KEY (`rev`) REFERENCES `revinfo` (`rev`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `items_aud`
+--
+
+LOCK TABLES `items_aud` WRITE;
+/*!40000 ALTER TABLE `items_aud` DISABLE KEYS */;
+/*!40000 ALTER TABLE `items_aud` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_detail`
+--
+
+DROP TABLE IF EXISTS `order_detail`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_detail` (
+  `id_items` bigint NOT NULL,
+  `id_order` bigint NOT NULL,
+  `prince` bigint DEFAULT NULL,
+  `quantity` int DEFAULT NULL,
+  PRIMARY KEY (`id_items`,`id_order`),
+  KEY `Order_fk` (`id_order`),
+  CONSTRAINT `Items_fk` FOREIGN KEY (`id_items`) REFERENCES `items` (`id_items`),
+  CONSTRAINT `Order_fk` FOREIGN KEY (`id_order`) REFERENCES `orders` (`id_order`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_detail`
+--
+
+LOCK TABLES `order_detail` WRITE;
+/*!40000 ALTER TABLE `order_detail` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_detail` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+  `id_order` bigint NOT NULL,
+  `order_date` datetime(6) NOT NULL,
+  `status` varchar(255) DEFAULT NULL,
+  `total` double DEFAULT NULL,
+  `id_user` bigint DEFAULT NULL,
+  PRIMARY KEY (`id_order`),
+  KEY `Order_user_fk` (`id_user`),
+  CONSTRAINT `Order_user_fk` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `revinfo`
+--
+
+DROP TABLE IF EXISTS `revinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `revinfo` (
+  `rev` int NOT NULL AUTO_INCREMENT,
+  `revtstmp` bigint DEFAULT NULL,
+  PRIMARY KEY (`rev`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `revinfo`
+--
+
+LOCK TABLES `revinfo` WRITE;
+/*!40000 ALTER TABLE `revinfo` DISABLE KEYS */;
+/*!40000 ALTER TABLE `revinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `id_user` bigint NOT NULL AUTO_INCREMENT,
+  `country` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `money` double NOT NULL,
+  `name_user` varchar(255) NOT NULL,
+  `phone` bigint NOT NULL,
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'hn','long',22000,'long',1),(2,'hn','l',22222,'bih',2);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-21 16:56:46
