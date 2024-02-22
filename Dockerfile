31839a9d34befc6938b2774b273cb55d7329c889
vietlong1011 <longlieuliny@gmail.com>
2024-02-23 01:08:06 +0700

    oke iP DB

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM eclipse-temurin:17-jdk-alpine
 LABEL authors="LONG"
 VOLUME /tmp
+EXPOSE 8088
 COPY target/*.jar demo-cicd-0.0.1-SNAPSHOT.jar
 ENTRYPOINT ["java", "-jar","demo-cicd-0.0.1-SNAPSHOT.jar"]
--- src/main/java/cicd/DemoCicdApplication.java
+++ src/main/java/cicd/DemoCicdApplication.java
@@ -1,5 +1,7 @@
 package cicd;
 
+import java.sql.Connection;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,8 @@ public class DemoCicdApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoCicdApplication.class, args);
+	
+		System.out.println("");
 		System.out.println("Test");
 	}
 
