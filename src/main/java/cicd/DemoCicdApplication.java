4849e9e6de8014f51993d1d0cfec2d3c48806fda
vietlong1011 <longlieuliny@gmail.com>
2024-03-04 04:38:04 +0700

    up

---
+++ src/main/java/cicd/DemoCicdApplication.java
@@ -0,0 +1,18 @@
+package cicd;
+
+import java.sql.Connection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCicdApplication.class, args);
+	
+		System.out.println("");
+		System.out.println("Test");
+	}
+
+}
